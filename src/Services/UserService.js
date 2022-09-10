5369c1516552104ea91adc27d25078825893b6db
gkishanrao <gkishanrao@gmail.com>
2022-09-09 22:19:16 -0400

    Create React Application

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const USER_API_BASE_URL= "http://localhost:8081/catalog/getCataLog";
 
+const USER_API_BASE_URL1= "http://localhost:8081/catalog/getCataLog";
+
 class UserService{
     getUserDetails(){
         return axios.get(USER_API_BASE_URL);
